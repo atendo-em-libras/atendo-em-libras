d214c1ee6171291edc44ab622fcb03f864f39e6d
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-05-28 17:10:38 -0300

    :bug:(App): remove undefined import to App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import {grommet, Grommet} from "grommet";
 import AppHeader from "./components/header/header";
 import { ProviderCard } from "./components/providerCard/ProviderCard";
