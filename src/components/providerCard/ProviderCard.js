4ae59d29b6bb1baaa2a3f1d647c2db6007deb31c
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-06-01 15:41:41 -0300

    :recycle:(CategoryBadge): remove CategoryBadge definition from ProviderCard

--- src/components/providerCard/ProviderCard.js
+++ src/components/providerCard/ProviderCard.js
@@ -1,45 +1,60 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import './ProviderCard.css'
+import { Card } from './Card'
+import { CategoryBadge } from './CategoryBadge';
+
 
 class ProviderCard extends React.Component {
-    render() {
-        const { name, category, specialty, licenseNumber, videoCallAvailability, phoneNumber, email } = this.props
-        return (
-            <div className='providerCard'>
-                { videoCallAvailability && 
-                <p className='providerVideoCallAvailability'>Atende Online</p> }
-                <div className='providerCardHeader'>
-                    <p className='providerCategory'>{ category }</p> 
-                    <p className='providerSpecialty'>{ specialty }</p>
-                </div>
-                <div className='providerCardBody'>
-                    <div className='providerCardColumn'>
-                        <p>{ name }</p>
-                        <p>REGISTRO</p>
-                        <p>{ licenseNumber }</p>
-                    </div>
-                    <div className='providerCardColumn'>
-                        <div className="columnBlock">
-                            <p>Contato</p>
-                            <p>{ phoneNumber }</p>
-                            <p>{ email }</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+
+  categoryColors = Object.freeze({
+    'medicina': '#2AC390',
+    'pessoa cuidadora': '#EAC276'
+  })
+
+  getCategoryColor = (categoryString) => {
+    categoryString = categoryString.trim().toLowerCase()
+    return this.categoryColors[categoryString]
+  }
+
+  render() {
+    const { name, category, specialty, licenseNumber, videoCallAvailability, phoneNumber, email } = this.props
+    return (
+      <Card className=''>
+        { videoCallAvailability && 
+        <p className='providerVideoCallAvailability'>Atende Online</p> }
+        <div className='providerCardHeader'>
+          <CategoryBadge category={category} />
+          
+          <p className='providerSpecialty'>{ specialty }</p>
+        </div>
+        <div className='providerCardBody'>
+          <div className='providerCardColumn'>
+            <p>{ name }</p>
+            <p>REGISTRO</p>
+            <p>{ licenseNumber }</p>
+          </div>
+          <div className='providerCardColumn'>
+              <div className="columnBlock">
+                  <p>Contato</p>
+                  <p>{ phoneNumber }</p>
+                  <p>{ email }</p>
+              </div>
+          </div>
+        </div>
+      </Card>
+    )
+  }
 }
 
 ProviderCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-    specialty: PropTypes.string.isRequired,
-    licenseNumber: PropTypes.string.isRequired,
-    videoCallAvailability: PropTypes.bool,
-    phoneNumber: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  specialty: PropTypes.string.isRequired,
+  licenseNumber: PropTypes.string.isRequired,
+  videoCallAvailability: PropTypes.bool,
+  phoneNumber: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired
 }
 
 export { ProviderCard };
