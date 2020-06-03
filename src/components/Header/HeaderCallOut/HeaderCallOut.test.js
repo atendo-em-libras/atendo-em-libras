26a7eca12f6fb7402a33a6e55e3986a03faa3cf5
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-06-03 13:02:06 -0300

    :white_check_mark:(HeaderCallOut): implement tests

--- src/components/Header/HeaderCallOut/HeaderCallOut.test.js
+++ src/components/Header/HeaderCallOut/HeaderCallOut.test.js
@@ -3,14 +3,14 @@ import { render } from "@testing-library/react"
 import { HeaderCallOut } from './HeaderCallOut'
 
 describe("HeaderCallOut component", () => {
-  let headerCallOutElement;
+  let headerCallOutContainer;
 
   beforeEach(() => {
-    headerCallOutElement = render(<HeaderCallOut />)
+    headerCallOutContainer = render(<HeaderCallOut />)
   });
 
   it("should render register message", () => {
-      expect(headerCallOutElement.getByText("Profissionais de diverssas áreas da saúde prontos para te atender")).toBeInTheDocument()
+      expect(headerCallOutContainer.getByText("Profissionais de diverssas áreas da saúde prontos para te atender")).toBeInTheDocument()
   });
 })
 
