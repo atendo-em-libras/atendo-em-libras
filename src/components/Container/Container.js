d090c1d0d6ceb539aef664c4ad1a33ea68980b59
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2020-06-03 17:31:39 -0300

    :construction:(Footer) Include images

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+  width: 100%;
   box-sizing: border-box;
   padding:  0 100px;
 `
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,66 @@
 import React, { PureComponent } from 'react'
-import styled from "styled-components";
+import styled from "styled-components"
 
-import { Container } from '../Container';
-import logo from '../../assets/images/img_atendo_em_libras_logo_blue.svg';
+import { Container } from '../Container'
+import atendoEmLibasLogo from '../../assets/images/img_atendo_em_libras_logo_blue.svg'
+import thoughtworksLogo from '../../assets/images/img_thoughtworks_logo_black.svg'
 
 class Footer extends PureComponent {
   render = () => {
     return (
       <div className={this.props.className}>
         <Container>
-          <img src={logo} alt={'Logo projeto Atendo em Libras'}></img>
+          <FooterContent>
+            <img src={atendoEmLibasLogo} alt={'Logo Atendo em Libras'}></img>
+            <FooterElement>
+              <p>Apoio:</p>
+              <img src={thoughtworksLogo} alt={'Logo Thoughtworks'}></img>
+            </FooterElement>
+            <FooterElement>
+              <p>Dúvidas? Entre em contato em:</p>
+              <p>atendoemlibras@gmail.com</p>
+            </FooterElement>
+          </FooterContent>
         </Container>
-      </div>
+      </div>  
     )
   }
 }
 
 const StyledFooter = styled(Footer)`
   width: 100%;
-  height: 30vh;
+  height: 20vh;
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  color: white;
+  background-color: #FFFFFF;
+  border-top: solid #0000001A 1px;
+`
+
+const FooterContent = styled.div`
   display: flex;
   flex-flow: row;
   align-items: center;
   justify-content: space-between;
-  color: white;
-  background-color: #FFFFFF;
+`
+
+const FooterElement = styled.div`
+  display: flex;
+  flex-flow: column;
+  align-content: center;
+  img {
+    height: 2em;
+  }
+  p {
+    color: black;
+    font-size: 2em;
+    font-weight: 400;
+  }
+  p:first-child {
+    font-weight: 600;
+    padding-bottom: .5em;
+  }
 `
 
 export { StyledFooter as Footer }
