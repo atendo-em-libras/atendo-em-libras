8841118c8df2148a7c7bd8f43dcde2b0872d38dc
marco-beduschi <je-0n-r0g-fea>
2020-06-18 17:43:47 -0300

    :white_check_mark:(ErrorCard) Reenable test scenario to verify error in CI

--- src/components/ErrorCard/ErrorCard.test.js
+++ src/components/ErrorCard/ErrorCard.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
 import { ErrorCard } from './ErrorCard'
+import 'jest-styled-components'
 
 describe('ErrorCard tests', () => {
-  xit('Render correctly', () => {
+  it('Render correctly', () => {
     let onClick = jest.fn()
     render(<ErrorCard onClick={onClick} />)
     expect(screen.getByRole('error')).toMatchSnapshot()
