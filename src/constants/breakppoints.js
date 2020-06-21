9551720059e8f3c80d757172dbf69916fb8d3aa5
marco-beduschi <je-0n-r0g-fea>
2020-06-20 23:27:21 -0300

    :recycle:(breakpoints) Update breakpoints object to not include key 'breakpoints'

--- src/constants/breakppoints.js
+++ src/constants/breakppoints.js
@@ -1,11 +1,9 @@
 export const BREAKPOINTS = {
-  breakpoints: {
-    small: {
-      value: 800,
-    },
-    medium: { value: 900 },
-    large: {
-      value: 3000,
-    },
+  small: {
+    value: 800,
+  },
+  medium: { value: 900 },
+  large: {
+    value: 3000,
   },
 }
