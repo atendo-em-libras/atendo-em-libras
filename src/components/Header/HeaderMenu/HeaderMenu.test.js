7d570dd8f0ec9bdaa51984cc27b70ee4efe01f9f
marco-beduschi <je-0n-r0g-fea>
2020-06-23 18:47:44 -0300

    :sparkles:(HeaderMenu) Add feature toggle and MainMenu modal on click

--- src/components/Header/HeaderMenu/HeaderMenu.test.js
+++ src/components/Header/HeaderMenu/HeaderMenu.test.js
@@ -1,15 +1,58 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { HeaderMenu } from './HeaderMenu'
+import { ResponsiveContext } from 'grommet'
+import { FeatureTogglesContext } from '../../../FeatureTogglesContext'
 
 describe('HeaderMenu component', () => {
-  let headerMenuContainer
+  let view
 
-  beforeEach(() => {
-    headerMenuContainer = render(<HeaderMenu />)
+  describe('when feature toggle SHOW_RESPONSIVE_HEADER is true', () => {
+    beforeEach(() => {
+      view = render(<HeaderMenu />)
+    })
+
+    it('should render register message', () => {
+      expect(view.getByText('É profissional de saúde ou conhece alguém da área?')).toBeInTheDocument()
+    })
+
+    describe('when mobile', () => {
+      it('displays a hamburger icon for main menu', () => {
+        view.rerender(
+          <FeatureTogglesContext.Provider value={{ SHOW_RESPONSIVE_HEADER: true }}>
+            <ResponsiveContext.Provider value={'small'}>
+              <HeaderMenu />
+            </ResponsiveContext.Provider>
+          </FeatureTogglesContext.Provider>
+        )
+
+        expect(view.getByRole('button', { name: 'Menu Button' })).toBeInTheDocument()
+      })
+
+      it('displays register button on modal', () => {
+        view.rerender(
+          <FeatureTogglesContext.Provider value={{ SHOW_RESPONSIVE_HEADER: true }}>
+            <ResponsiveContext.Provider value={'small'}>
+              <HeaderMenu />
+            </ResponsiveContext.Provider>
+          </FeatureTogglesContext.Provider>
+        )
+
+        const button = view.getByRole('button', { name: 'Menu Button' })
+
+        expect(view.queryByRole('button', { name: 'Cadastre Aqui' })).toBeNull()
+        fireEvent.click(button)
+        expect(view.getByRole('button', { name: 'Cadastre Aqui' })).toBeInTheDocument()
+      })
+    })
   })
+  describe('when feature toggle SHOW_RESPONSIVE_HEADER is false', () => {
+    beforeEach(() => {
+      view = render(<HeaderMenu />)
+    })
 
-  it('should render register message', () => {
-    expect(headerMenuContainer.getByText('É profissional de saúde ou conhece alguém da área?')).toBeInTheDocument()
+    it('should render register message', () => {
+      expect(view.getByText('É profissional de saúde ou conhece alguém da área?')).toBeInTheDocument()
+    })
   })
 })
--- src/components/Header/HeaderMobile/HeaderMobile.js
+++ src/components/Header/HeaderMobile/HeaderMobile.js
@@ -2,22 +2,50 @@ import React, { PureComponent } from 'react'
 import styled from 'styled-components'
 import { PrimaryButton } from '../../Buttons'
 import { Layer } from 'grommet'
+import logo from '../../../assets/images/img_atendo_em_libras_logo_white.svg'
 
 const StyledButton = styled(PrimaryButton)`
+  color: #5996f7;
   border-radius: 20px 20px 20px 0;
+  background-color: white;
 `
 
 const MainMenuModal = styled(Layer)`
-  background-color: red;
+  background-color: #5996f7;
 `
 
+const Logo = styled.img`
+  @media (max-width: 800px) {
+    height: auto;
+    width: 40%;
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 30px;
+`
+
+const Header = styled.div``
+
+const Footer = styled.div``
 class HeaderMobile extends PureComponent {
   render = () => {
     return (
       <MainMenuModal modal plain full>
-        <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/h1sX9nD45PgUAzGB6">
-          <StyledButton>Cadastre Aqui</StyledButton>
-        </a>
+        <Wrapper>
+          <Header>
+            <Logo src={logo} alt={'Logo projeto Atendo em Libras'} />
+          </Header>
+          <Footer>
+            <a target="_blank" rel="noopener noreferrer" href="https://forms.gle/h1sX9nD45PgUAzGB6">
+              <StyledButton>Cadastre Aqui</StyledButton>
+            </a>
+          </Footer>
+        </Wrapper>
       </MainMenuModal>
     )
   }
