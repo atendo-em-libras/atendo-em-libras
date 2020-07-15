db10becf573ac37a715bcf8768f09c317606d4ba
marco-beduschi <je-0n-r0g-fea>
2020-07-15 09:58:13 -0300

    :sparkles:(Index) Testing Google Analytics implementation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Router } from 'react-router-dom'
 import history from './history'
+import ReactGA from 'react-ga'
 
+ReactGA.initialize('UA-172748177-1')
 ReactDOM.render(
   <React.StrictMode>
     <Router history={history}>
