5604c33ea5d41bbf47b9e6d6ee25d0567d446fdc
felipefahrion <felipe.fahrion@gmail.com>
2020-07-17 15:44:28 -0300

    :sparkles:(About) add path about page and created about page @nicoledbianchin @andressaneumann

---
+++ src/pages/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Sobre o projeto</h1>
+      </>
+    )
+  }
+}
+
+export { About }
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Home } from '../pages/Home/Home'
 import { withAnalytics } from '../hocs/withAnalytics'
+import { About } from '../pages/About/About'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={withAnalytics(Home)} />
+      <Route path="/sobre" component={withAnalytics(About)} />
     </Switch>
   </BrowserRouter>
 )
