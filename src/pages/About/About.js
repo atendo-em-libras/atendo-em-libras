f91dca96d52a4801d57641bc3727de553c2a0ca0
marco-beduschi <je-0n-r0g-fea>
2020-07-20 11:33:36 -0300

    :construction:(README) Add environment variables session

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AboutBanner } from './AboutBanner/AboutBanner'
 import styled from 'styled-components/macro'
+import { Grommet, Paragraph } from 'grommet'
 
 const AboutWrapper = styled.div`
   display: flex;
@@ -13,6 +14,15 @@ const AboutWrapper = styled.div`
   text-align: left;
 `
 
+const SectionStyled = styled.section`
+  flex-flow: in-line;
+  align-items: center;
+  width: 100%;
+  height: 400px;
+  font-family: Open Sans, sans-serif;
+  text-align: left;
+`
+
 class About extends React.Component {
   render() {
     return (
@@ -20,6 +30,31 @@ class About extends React.Component {
         <AboutWrapper>
           <AboutBanner />
         </AboutWrapper>
+
+        <SectionStyled>
+          <h1>O Atendo em Libras</h1>
+          <Paragraph>
+            O Atendo em Libras é uma plataforma de cadastro e consulta de profissionais que sabem Libras, com a
+            finalidade de disponibilizar à comunidade surda acesso aos serviços sem a necessidade de intérprete. Assim,
+            as pessoas surdas podem cuidar de sua saúde com autonomia.
+          </Paragraph>
+        </SectionStyled>
+
+        <SectionStyled>
+          <h1>Nosso propósito</h1>
+          <Paragraph>
+            Dar visibilidade aos profissionais que disponibilizam serviços de atendimento em Libras, trazendo
+            acessibilidade à comunidade surda e, também, conceder a autonomia às pessoas surdas nos cuidados de saúde.
+          </Paragraph>
+        </SectionStyled>
+
+        <SectionStyled>
+          <h1>As idealizadoras</h1>
+        </SectionStyled>
+
+        <SectionStyled>
+          <h1>Conheça quem faz parte</h1>
+        </SectionStyled>
       </>
     )
   }
