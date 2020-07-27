b39d9a86f3dd510305b3037275f9e0846e6577d9
marco-beduschi <je-0n-r0g-fea>
2020-07-27 11:00:30 -0300

    :lipstick:(ProviderList) refactor to functional component

--- src/components/ProviderCard/ProviderCard.js
+++ src/components/ProviderCard/ProviderCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import {
   SectionTitle,
   License,
@@ -19,184 +19,179 @@ import { videoIcon, phoneIcon, emailIcon, addressIcon, healthInsuranceIcon } fro
 import { ResponsiveGrid } from '../ResponsiveGrid'
 import { ToggleButton } from './ToggleButton'
 
-class ProviderCard extends Component {
-  constructor(props) {
-    super(props)
+const ProviderCard = (props) => {
+  const [open, setOpen] = useState(false)
 
-    this.state = {
-      open: false,
-    }
+  const columns = {
+    small: ['auto'],
+    medium: ['1/2', '1/2'],
+    large: ['1/2', '1/2'],
+    xlarge: ['1/2', '1/2'],
   }
 
-  render() {
-    const columns = {
-      small: ['auto'],
-      medium: ['1/2', '1/2'],
-      large: ['1/2', '1/2'],
-      xlarge: ['1/2', '1/2'],
-    }
+  const rows = {
+    small: ['auto', 'auto', 'auto'],
+    medium: ['auto', 'auto'],
+    large: ['auto', 'auto'],
+    xlarge: ['auto', 'auto'],
+  }
 
-    const rows = {
-      small: ['auto', 'auto', 'auto'],
-      medium: ['auto', 'auto'],
-      large: ['auto', 'auto'],
-      xlarge: ['auto', 'auto'],
-    }
+  const fixedGridAreas = {
+    small: [
+      { name: 'header', start: [0, 0], end: [0, 0] },
+      { name: 'first', start: [0, 1], end: [0, 1] },
+      { name: 'second', start: [0, 2], end: [0, 2] },
+    ],
+    medium: [
+      { name: 'header', start: [0, 0], end: [1, 0] },
+      { name: 'first', start: [0, 1], end: [0, 1] },
+      { name: 'second', start: [1, 1], end: [1, 1] },
+    ],
+    large: [
+      { name: 'header', start: [0, 0], end: [1, 0] },
+      { name: 'first', start: [0, 1], end: [0, 1] },
+      { name: 'second', start: [1, 1], end: [1, 1] },
+    ],
+    xlarge: [
+      { name: 'header', start: [0, 0], end: [1, 0] },
+      { name: 'first', start: [0, 1], end: [0, 1] },
+      { name: 'second', start: [1, 1], end: [1, 1] },
+    ],
+  }
 
-    const fixedGridAreas = {
-      small: [
-        { name: 'header', start: [0, 0], end: [0, 0] },
-        { name: 'first', start: [0, 1], end: [0, 1] },
-        { name: 'second', start: [0, 2], end: [0, 2] },
-      ],
-      medium: [
-        { name: 'header', start: [0, 0], end: [1, 0] },
-        { name: 'first', start: [0, 1], end: [0, 1] },
-        { name: 'second', start: [1, 1], end: [1, 1] },
-      ],
-      large: [
-        { name: 'header', start: [0, 0], end: [1, 0] },
-        { name: 'first', start: [0, 1], end: [0, 1] },
-        { name: 'second', start: [1, 1], end: [1, 1] },
-      ],
-      xlarge: [
-        { name: 'header', start: [0, 0], end: [1, 0] },
-        { name: 'first', start: [0, 1], end: [0, 1] },
-        { name: 'second', start: [1, 1], end: [1, 1] },
-      ],
-    }
+  const {
+    name,
+    category,
+    specialty,
+    licenseNumber,
+    videoCallAvailability,
+    phoneNumber,
+    email,
+    experience,
+    healthInsurance,
+    city,
+    videoCallPlatform,
+    state,
+    address,
+  } = props.provider
 
-    const {
-      name,
-      category,
-      specialty,
-      licenseNumber,
-      videoCallAvailability,
-      phoneNumber,
-      email,
-      experience,
-      healthInsurance,
-      city,
-      videoCallPlatform,
-      state,
-      address,
-    } = this.props.provider
+  const ProviderVideoCallAvailabilityLabel = () => (
+    <ProviderVideoCallAvailabilityContainer>
+      <ProviderVideoCallAvailability role="note">
+        <IconText src={videoIcon} alt="video icon" />
+        Atende Online
+      </ProviderVideoCallAvailability>
+    </ProviderVideoCallAvailabilityContainer>
+  )
 
-    const ProviderVideoCallAvailabilityLabel = () => (
-      <ProviderVideoCallAvailabilityContainer>
-        <ProviderVideoCallAvailability role="note">
-          <IconText src={videoIcon} alt="video icon" />
-          Atende Online
-        </ProviderVideoCallAvailability>
-      </ProviderVideoCallAvailabilityContainer>
-    )
-
-    const DetailsInfo = () => (
-      <>
-        <ProviderName>{name}</ProviderName>
-        <ProviderSpecialty>{specialty}</ProviderSpecialty>
-        <License>
-          <p className="title">REGISTRO</p>
-          {licenseNumber ? <p className="number">{licenseNumber}</p> : <p className="number">-</p>}
-        </License>
-        {experience && <ExerienceText size="small">{experience}</ExerienceText>}
-        <SectionBlock>
-          {state && (
-            <SectionTitle>
-              <IconText src={addressIcon} alt="address icon" />
-              {city && `${city} - `}
-              {`${state}`}
-            </SectionTitle>
-          )}
-          <Paragraph size="small">{address}</Paragraph>
-        </SectionBlock>
-      </>
-    )
-
-    const ContactInfo = () => (
+  const DetailsInfo = () => (
+    <>
+      <ProviderName>{name}</ProviderName>
+      <ProviderSpecialty>{specialty}</ProviderSpecialty>
+      <License>
+        <p className="title">REGISTRO</p>
+        {licenseNumber ? <p className="number">{licenseNumber}</p> : <p className="number">-</p>}
+      </License>
+      {experience && <ExerienceText size="small">{experience}</ExerienceText>}
       <SectionBlock>
-        <SectionTitle>Contato</SectionTitle>
-        <p>
-          <IconText src={phoneIcon} alt="phone icon" />
-          <span> {phoneNumber}</span>
-        </p>
-        <Paragraph size="small">
-          <IconText src={emailIcon} alt="e-mail icon" />
-          <span> {email}</span>
-        </Paragraph>
+        {state && (
+          <SectionTitle>
+            <IconText src={addressIcon} alt="address icon" />
+            {city && `${city} - `}
+            {`${state}`}
+          </SectionTitle>
+        )}
+        <Paragraph size="small">{address}</Paragraph>
       </SectionBlock>
-    )
+    </>
+  )
 
-    const CallPlatform = () => (
-      <SectionBlock>
-        <SectionTitle>Atendimento online</SectionTitle>
-        <p>
-          <IconText src={videoIcon} alt="video icon" />
-          {videoCallPlatform ? <span>{videoCallPlatform}</span> : <span>Não informado</span>}
-        </p>
-      </SectionBlock>
-    )
+  const ContactInfo = () => (
+    <SectionBlock>
+      <SectionTitle>Contato</SectionTitle>
+      <p>
+        <IconText src={phoneIcon} alt="phone icon" />
+        <span> {phoneNumber}</span>
+      </p>
+      <Paragraph size="small">
+        <IconText src={emailIcon} alt="e-mail icon" />
+        <span> {email}</span>
+      </Paragraph>
+    </SectionBlock>
+  )
 
-    const HealthInsurance = () => (
-      <SectionBlock>
-        <SectionTitle>Planos de saúde</SectionTitle>
-        <p>
-          <IconText src={healthInsuranceIcon} alt="card icon" />
-          {healthInsurance ? <span>{healthInsurance}</span> : <span>Não informado</span>}
-        </p>
-      </SectionBlock>
-    )
+  const CallPlatform = () => (
+    <SectionBlock>
+      <SectionTitle>Atendimento online</SectionTitle>
+      <p>
+        <IconText src={videoIcon} alt="video icon" />
+        {videoCallPlatform ? <span>{videoCallPlatform}</span> : <span>Não informado</span>}
+      </p>
+    </SectionBlock>
+  )
 
-    const MoreInfoBox = () => (
-      <>
-        {videoCallAvailability && <CallPlatform />}
-        <HealthInsurance />
-      </>
-    )
+  const HealthInsurance = () => (
+    <SectionBlock>
+      <SectionTitle>Planos de saúde</SectionTitle>
+      <p>
+        <IconText src={healthInsuranceIcon} alt="card icon" />
+        {healthInsurance ? <span>{healthInsurance}</span> : <span>Não informado</span>}
+      </p>
+    </SectionBlock>
+  )
 
-    return (
-      <ProviderCardWrapper className="providerCard" role="provider">
-        <ResponsiveGrid columns={columns} rows={rows} areas={fixedGridAreas}>
-          <Box gridArea="header">
-            {videoCallAvailability && <ProviderVideoCallAvailabilityLabel />}
-            <CategoryBadge category={category} />
-          </Box>
+  const MoreInfoBox = () => (
+    <>
+      {videoCallAvailability && <CallPlatform />}
+      <HealthInsurance />
+    </>
+  )
 
-          <ResponsiveContext.Consumer>
-            {(responsive) =>
-              responsive === 'small' ? (
+  return (
+    <ProviderCardWrapper className="providerCard" role="provider">
+      <ResponsiveGrid columns={columns} rows={rows} areas={fixedGridAreas}>
+        <Box gridArea="header">
+          {videoCallAvailability && <ProviderVideoCallAvailabilityLabel />}
+          <CategoryBadge category={category} />
+        </Box>
+
+        <ResponsiveContext.Consumer>
+          {(responsive) =>
+            responsive === 'small' ? (
+              <Box gridArea="first" className="providerCardColumn">
+                <DetailsInfo />
+                <ContactInfo />
+                <Collapsible open={open} {...props}>
+                  <MoreInfoBox />
+                </Collapsible>
+                <ToggleButton
+                  isOpen={open}
+                  labelClose="Fechar"
+                  labelOpen="Saiba Mais"
+                  iconClose={FormUp}
+                  iconOpen={FormDown}
+                  onClick={() => {
+                    console.log(open)
+                    setOpen(!open)
+                  }}
+                />
+              </Box>
+            ) : (
+              <>
                 <Box gridArea="first" className="providerCardColumn">
                   <DetailsInfo />
-                  <ContactInfo />
-                  <Collapsible open={this.state.open} {...this.props}>
-                    <MoreInfoBox />
-                  </Collapsible>
-                  <ToggleButton
-                    isOpen={this.state.open}
-                    labelClose="Fechar"
-                    labelOpen="Saiba Mais"
-                    iconClose={FormUp}
-                    iconOpen={FormDown}
-                    onClick={() => this.setState({ open: !this.state.open })}
-                  />
                 </Box>
-              ) : (
-                <>
-                  <Box gridArea="first" className="providerCardColumn">
-                    <DetailsInfo />
-                  </Box>
-                  <Box gridArea="second" className="providerCardColumn">
-                    <ContactInfo />
-                    <MoreInfoBox />
-                  </Box>
-                </>
-              )
-            }
-          </ResponsiveContext.Consumer>
-        </ResponsiveGrid>
-      </ProviderCardWrapper>
-    )
-  }
+                <Box gridArea="second" className="providerCardColumn">
+                  <ContactInfo />
+                  <MoreInfoBox />
+                </Box>
+              </>
+            )
+          }
+        </ResponsiveContext.Consumer>
+      </ResponsiveGrid>
+    </ProviderCardWrapper>
+  )
 }
 
 ProviderCard.propTypes = {
