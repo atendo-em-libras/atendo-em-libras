f8bee41690033e8613816904c76b8eece6c6b114
marco-beduschi <je-0n-r0g-fea>
2020-08-12 10:40:19 -0300

    :sparkles:(HeaderNavModal) add nav links to mobile navbar

--- src/components/Header/HeaderNavModal/HeaderNavModal.js
+++ src/components/Header/HeaderNavModal/HeaderNavModal.js
@@ -4,7 +4,21 @@ import { Box, Heading } from 'grommet'
 import logo from '../../../assets/images/img_atendo_em_libras_logo_all_white.svg'
 import { closeIcon } from '../../../assets/icons'
 import { StyledButton, Modal, Logo } from './HeaderNavModalStyles'
+import styled from 'styled-components'
+import { AnchorLink } from '../../AnchorLink/AnchorLink'
 
+const List = styled.ol`
+  display: flex;
+  flex-direction: column;
+`
+
+const ListItem = styled.li`
+  margin-bottom: 40px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
 class HeaderNavModal extends PureComponent {
   constructor(props) {
     super(props)
@@ -30,6 +44,18 @@ class HeaderNavModal extends PureComponent {
               icon={<img src={closeIcon} alt="Close Icon" width="18px" height="18px" />}
               size="small"
             ></Button>
+          </Box>
+          <Box flex="grow" pad={{ top: 'xlarge' }}>
+            <nav>
+              <List>
+                <ListItem>
+                  <AnchorLink to="/" label="Home" size="medium" />
+                </ListItem>
+                <ListItem>
+                  <AnchorLink to="/sobre" label="Sobre o Projeto" size="medium" />
+                </ListItem>
+              </List>
+            </nav>
           </Box>
           <Box align="start">
             <Heading level="3" color="white" margin={{ bottom: 'medium' }}>
--- src/components/Header/HeaderNavModal/HeaderNavModal.test.js
+++ src/components/Header/HeaderNavModal/HeaderNavModal.test.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { render, fireEvent } from '@testing-library/react'
 import { HeaderNavModal } from './HeaderNavModal'
 import 'jest-styled-components'
+import { Router } from 'react-router-dom'
+import { FeatureTogglesContext } from '../../../FeatureTogglesContext'
+import { createMemoryHistory } from 'history'
 
 describe('HeaderNavModal component', () => {
+  const history = createMemoryHistory()
   let view
   let onClose
 
   beforeEach(() => {
     onClose = jest.fn()
-    view = render(<HeaderNavModal onClose={onClose} />)
+    view = render(
+      <Router history={history}>
+        <HeaderNavModal onClose={onClose} />
+      </Router>
+    )
   })
 
   it('should execute callback function when close button is clicked', () => {
@@ -18,6 +26,20 @@ describe('HeaderNavModal component', () => {
     expect(closeButton).toBeInTheDocument()
     fireEvent.click(closeButton)
     expect(onClose).toHaveBeenCalled()
+  })
+
+  it("menu displays a link to 'Home' that takes the user to '/'", () => {
+    history.push('/some-route')
+    fireEvent.click(view.getByText(/Home/i))
+
+    expect(history.location.pathname).toBe('/')
+  })
+
+  it("menu displays a link to 'Sobre o Projeto' that takes the user to '/about'", () => {
+    history.push('/some-route')
+    fireEvent.click(view.getByText(/Sobre o Projeto/i))
+
+    expect(history.location.pathname).toBe('/sobre')
   })
 
   it('should redirect to form when register button is clicked', () => {
