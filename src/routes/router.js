1e5a2969c850c9f5a753c152ce1dcd303c673459
Ramon Costa <ramoncosta@Ramons-MacBook-Pro.local>
2020-08-25 14:57:19 -0300

    :sparkles: (SignUp) create SignUp page

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,12 +5,14 @@ import { withAnalytics } from '../hocs/withAnalytics'
 import { withAnalyticsAndHeader } from '../hocs/withAnalyticsAndHeader'
 import { About } from '../pages/About/About'
 import { NotFound } from '../pages/NotFound/NotFound'
+import { SignUp } from '../pages/SignUp/SignUp'
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={withAnalyticsAndHeader(Home)} />
       <Route path="/sobre" component={withAnalyticsAndHeader(About)} />
+      <Route path="/cadastrar" component={withAnalyticsAndHeader(SignUp)} />
       <Route component={withAnalytics(NotFound)} />
     </Switch>
   </BrowserRouter>
