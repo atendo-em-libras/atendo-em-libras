f08e59c8522a8cf32b2de759cdd873597c82485f
Felipe Fahrion <felipefahrion@Felipes-MacBook-Pro.local>
2020-09-05 19:21:12 -0300

    :chores:(ProviderList): update the tests

--- src/components/ProviderList/ProviderList.test.js
+++ src/components/ProviderList/ProviderList.test.js
@@ -8,7 +8,7 @@ jest.mock('../../api/provider.js')
 
 describe('ProviderList tests', () => {
   it('Component should render a provider card when showCard column is yes', async () => {
-    //ProviderApi.get.mockResolvedValue(mockProviders.providers)
+    // ProviderApi.get.mockResolvedValue(mockProviders.providers)
 
     render(<ProviderList />)
 
@@ -16,17 +16,17 @@ describe('ProviderList tests', () => {
     expect(providerList).toBeInTheDocument()
   })
 
-  // it('Component should not render a provider card when showCard column is no', async () => {
-  //   ProviderApi.get.mockResolvedValue(mockProviders.providers)
+  it('Component should not render a provider card when showCard column is no', async () => {
+    // ProviderApi.get.mockResolvedValue(mockProviders.providers)
 
-  //   render(<ProviderList />)
+    render(<ProviderList />)
 
-  //   const providerList = await screen.findAllByText('Contato')
-  //   expect(providerList.length).toBeLessThanOrEqual(2)
-  // })
+    const providerList = await screen.findAllByText('Contato')
+    expect(providerList.length).toBeLessThanOrEqual(2)
+  })
 
   // it('Component should render error to find providers', async () => {
-  //   ProviderApi.get.mockResolvedValue([])
+  //   // ProviderApi.get.mockResolvedValue([])
 
   //   render(<ProviderList />)
 
@@ -34,22 +34,22 @@ describe('ProviderList tests', () => {
   //   expect(providerList).toBeInTheDocument()
   // })
 
-  // it('Component should show video call availability', async () => {
-  //   ProviderApi.get.mockResolvedValue(mockProviders.providers)
+  it('Component should show video call availability', async () => {
+    // ProviderApi.get.mockResolvedValue(mockProviders.providers)
 
-  //   render(<ProviderList />)
+    render(<ProviderList />)
 
-  //   const videoCallAvailability = await screen.findAllByText('Atende Online')
-  //   expect(videoCallAvailability[0]).toBeInTheDocument()
-  // })
+    const videoCallAvailability = await screen.findAllByText('Atende Online')
+    expect(videoCallAvailability[0]).toBeInTheDocument()
+  })
 
-  // it('Component should show empty space before loading providers', async () => {
-  //   ProviderApi.get.mockResolvedValue(mockProviders.providers)
+  it('Component should show empty space before loading providers', async () => {
+    // ProviderApi.get.mockResolvedValue(mockProviders.providers)
 
-  //   render(<ProviderList />)
+    render(<ProviderList />)
 
-  //   expect(screen.queryByRole('provider')).toBeNull()
-  //   expect(screen.getByTestId('emptyList')).toBeInTheDocument()
-  //   expect(await screen.findByText('doctor-who@tardis.com')).toBeInTheDocument()
-  // })
+    expect(screen.queryByRole('provider')).toBeNull()
+    expect(screen.getByTestId('emptyList')).toBeInTheDocument()
+    expect(await screen.findByText('doctor-who@tardis.com')).toBeInTheDocument()
+  })
 })
