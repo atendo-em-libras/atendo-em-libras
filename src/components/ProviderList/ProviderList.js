1733a80df3b4735832872347c201cc83fafcab80
marco-beduschi <je-0n-r0g-fea>
2020-09-14 17:19:16 -0300

    :bug: Remove whitespace between cards in the home page

--- src/components/ProviderList/ProviderList.js
+++ src/components/ProviderList/ProviderList.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react'
-import { ResponsiveGrid } from '../ResponsiveGrid'
 import { ProviderCard } from '../ProviderCard'
 import { ErrorCard } from '../ErrorCard'
 import ProviderApi from '../../api/provider'
+import styled from 'styled-components'
+import { Box } from 'grommet'
+import { respondTo } from '../../utils/breakpoints/_respondTo'
 
-const columns = {
-  small: ['auto'],
-  medium: ['auto'],
-  large: ['auto', 'auto'],
-}
+const cardGap = '30px'
 
-const rows = {
-  small: ['auto'],
-  medium: ['auto'],
-  large: ['auto'],
-}
+const ColumnsContainer = styled.div`
+  display: block;
 
-const fixedGridAreas = {
-  small: [{ name: 'card', start: [0, 0], end: [0, 0] }],
-  medium: [{ name: 'card', start: [0, 0], end: [0, 0] }],
-  large: [{ name: 'card', start: [0, 0], end: [1, 0] }],
-}
+  ${respondTo.desktop`
+    columns: 2;
+    column-gap: ${cardGap};
+  `}
+`
+
+const Column = styled(Box)`
+  break-inside: avoid;
+`
 
 class ProviderList extends Component {
   constructor(props) {
@@ -54,11 +53,17 @@ class ProviderList extends Component {
     }
     console.log(providers)
     return providers && providers.length > 0 ? (
-      <ResponsiveGrid columns={columns} rows={rows} areas={fixedGridAreas} justify="center" gapRow="50px" gapCol="50px">
+      <ColumnsContainer>
         {providers.map((provider) => {
-          return <ProviderCard key={provider.id} gridArea="card" provider={provider} />
+          return (
+            <Column>
+              <Box margin={{ bottom: cardGap }}>
+                <ProviderCard key={provider.id} provider={provider} />
+              </Box>
+            </Column>
+          )
         })}
-      </ResponsiveGrid>
+      </ColumnsContainer>
     ) : (
       <ErrorCard onClick={this.handleClick} />
     )
