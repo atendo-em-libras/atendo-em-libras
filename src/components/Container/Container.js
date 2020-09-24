c0fad9ab13ddf1138c6d6bb438ab8ef43dfc205b
Amilton Vanderlei Baracy Junior <laamilton.a@MacBook-Pro-de-Amilton.local>
2020-09-24 17:18:41 -0300

    :lipstick:(Container): Added the padding bottom on Container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -4,11 +4,11 @@ import styled from 'styled-components'
 import { respondTo } from '../../utils/breakpoints/_respondTo'
 
 const StyledBox = styled(Box)`
-  padding: 0 10vw;
+  padding: 0 10vw 10vw 10vw;
   margin: 0 auto;
 
   ${respondTo.desktop`
-    padding: 0 40px;
+    padding: 0 40px 50px 40px;
   `}
 `
 const Container = (props) => {
