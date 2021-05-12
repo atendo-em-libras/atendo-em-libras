d8d47289b60f8350086c4b0bd564e49e8b820d88
Daniela.jardim <daniela.jardim@MacBook-Pro-de-Danielajardim.local>
2021-05-12 13:57:48 -0300

    testes Filter

--- src/components/ProviderList/Filter/Filter.test.js
+++ src/components/ProviderList/Filter/Filter.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { findByText, fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Filter, { FilterChip } from './Filter'
 
 window.scrollTo = jest.fn()
