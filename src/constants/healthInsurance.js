62a4504ba726e8c5133b97e4d9279667e28f94aa
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-07-22 15:23:48 -0300

    :construction: Includes more health insurance options

--- src/constants/healthInsurance.js
+++ src/constants/healthInsurance.js
@@ -1,11 +1,51 @@
 export const healthInsurance = [
   {
-    Id: 'SULAMÉRICA',
-    Description: 'SulAmérica',
+    Id: 'SULAMERICA',
+    Description: 'SulAmerica',
   },
   {
     Id: 'UNIMED',
     Description: 'Unimed',
+  },
+  {
+    Id: 'BRADESCO',
+    Description: 'Bradesco',
+  },
+  {
+    Id: 'AMIL',
+    Description: 'Amil',
+  },
+  {
+    Id: 'NOTREDAME',
+    Description: 'NotreDame',
+  },
+  {
+    Id: 'CAIXA SEGURADORA',
+    Description: 'Caixa Seguradora',
+  },
+  {
+    Id: 'DR EXAME',
+    Description: 'Dr Exame',
+  },
+  {
+    Id: 'SAUDESC',
+    Description: 'Saudesc',
+  },
+  {
+    Id: 'INSTITUTO MAIS SAÚDE',
+    Description: 'Instituto Mais Saúde',
+  },
+  {
+    Id: 'CARE PLUS',
+    Description: 'Care Plus',
+  },
+  {
+    Id: 'GOLDEN CROSS',
+    Description: 'Golden Cross',
+  },
+  {
+    Id: 'IPASGO',
+    Description: 'Ipasgo',
   },
   {
     Id: 'PARTICULAR',
